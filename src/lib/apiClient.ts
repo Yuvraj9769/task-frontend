8230977d4421d26493a8905a093114d77ccff86e
Yuvraj Salte <yuvrajs22it@student.mes.ac.in>
2026-06-17 02:05:28 +0530

    fix: fixed apiClent env issue

--- src/lib/apiClient.ts
+++ src/lib/apiClient.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import type { ApiErrorResponse } from "@/types/caterer";
 
 const baseURL =
-  process.env.NEXT_PUBLIC_API_BASE_URL  ?? "http://localhost:5000/api/v1";
+  process.env.NEXT_PUBLIC_API_BASE_URL
 
   if (!baseURL) {
     throw new Error(
